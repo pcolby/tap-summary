a89a4b65b30e1d3ff8737a4b94e9e90d587dd294
Paul Colby <git@colby.id.au>
2022-07-23 00:03:39 +1000

    Avoid the use of `"${c^^}"` substitution
    Once again, due to the stupidly old version of Bash on macOS.

--- test/test.sh
+++ test/test.sh
@@ -4,21 +4,10 @@ set -o errexit -o noclobber -o nounset -o pipefail
 
 : "${SOURCE_DIR:=${BASH_SOURCE[0]%/*}}"
 
-function require {
-  local C;
-  for c in "$@"; do
-    if [ -v "${c^^}" ]; then continue; fi
-    C=$(command -v "$c") || { echo "Required command not found: $c" >&2; exit 1; }
-    declare -gr "${c^^}"="$C"
-  done
-}
-
-require diff find gawk
-
 failures=0
 while IFS= read -d '' -r dirName; do
   echo "Test case: ${dirName##*/}"
-  "$GAWK" -f "$SOURCE_DIR/../test-summary.gawk" --sandbox -- "$dirName"/*/{*.tap,test/unit/*.tap} >| "$dirName/observed.md"
-  "$DIFF" --color=auto --strip-trailing-cr --unified "$dirName/expected.md" "$dirName/observed.md" || ((++failures))
-done < <("$FIND" "$SOURCE_DIR" -maxdepth 1 -mindepth 1 -type d -print0)
+  gawk -f "$SOURCE_DIR/../test-summary.gawk" --sandbox -- "$dirName"/*/{*.tap,test/unit/*.tap} >| "$dirName/observed.md"
+  diff --color=auto --strip-trailing-cr --unified "$dirName/expected.md" "$dirName/observed.md" || ((++failures))
+done < <(find "$SOURCE_DIR" -maxdepth 1 -mindepth 1 -type d -print0)
 [[ "$failures" -eq 0 ]]
