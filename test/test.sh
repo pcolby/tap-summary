3af6e98f22c79843ff82c40f6b24fe5cee7a99bb
Paul Colby <git@colby.id.au>
2024-02-11 15:02:19 +1100

    Use globstar to locate TAP files in tests
    In preparation for adding a test suite for issue #2.

--- test/test.sh
+++ test/test.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 set -o errexit -o noclobber -o nounset -o pipefail
+shopt -s globstar inherit_errexit
 
 : "${SOURCE_DIR:=${BASH_SOURCE[0]%/*}}"
 
 failures=0
 while IFS= read -d '' -r dirName; do
   echo "Test case: ${dirName##*/}"
-  gawk -f "$SOURCE_DIR/../summary.gawk" --sandbox -- "$dirName"/*/{*.tap,test/unit/*.tap} >| "$dirName/observed.md"
+  gawk -f "$SOURCE_DIR/../summary.gawk" --sandbox -- "$dirName"/**/*.tap >| "$dirName/observed.md"
   case "$OSTYPE" in
     darwin*) extraDiffArgs='' ;;                                 # macOS
     msys*)   extraDiffArgs='--color=auto --strip-trailing-cr' ;; # Windows
