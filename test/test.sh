c828f60919b4f7ca5b61c248a9247531fbecab0a
Paul Colby <git@colby.id.au>
2024-02-11 15:43:18 +1100

    Don't rely on globstar since its not supported on macOS
    At least of the super-old Bash version that ships with macOS by
    default.

--- test/test.sh
+++ test/test.sh
@@ -3,14 +3,14 @@
 # SPDX-License-Identifier: MIT
 
 set -o errexit -o noclobber -o nounset -o pipefail
-shopt -s globstar inherit_errexit
 
 : "${SOURCE_DIR:=${BASH_SOURCE[0]%/*}}"
 
 failures=0
 while IFS= read -d '' -r dirName; do
   echo "Test case: ${dirName##*/}"
-  gawk -f "${SOURCE_DIR}/../summary.gawk" --sandbox -- "${dirName}"/**/*.tap >| "${dirName}/observed.md"
+  readarray -td '' tapFiles < <(find "${dirName}" -name '*.tap' -type f -d -print0 || :)
+  gawk -f "${SOURCE_DIR}/../summary.gawk" --sandbox -- "${tapFiles[@]}" >| "${dirName}/observed.md"
   case "${OSTYPE}" in
     darwin*) extraDiffArgs='' ;;                                 # macOS
     msys*)   extraDiffArgs='--color=auto --strip-trailing-cr' ;; # Windows
