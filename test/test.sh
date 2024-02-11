ac09c9bc76c5190cfc9d80197251bc9b6fd8fb90
Paul Colby <git@colby.id.au>
2024-02-11 16:08:52 +1100

    Use an array for extraDiffArgs

--- test/test.sh
+++ test/test.sh
@@ -13,11 +13,10 @@ while IFS= read -d '' -r dirName; do
   while IFS='' read -r file; do tapFiles+=("${file}"); done < <(find "${dirName}" -name '*.tap' -type f -d -print || :)
   gawk -f "${SOURCE_DIR}/../summary.gawk" --sandbox -- "${tapFiles[@]}" >| "${dirName}/observed.md"
   case "${OSTYPE}" in
-    darwin*) extraDiffArgs='' ;;                                 # macOS
-    msys*)   extraDiffArgs='--color=auto --strip-trailing-cr' ;; # Windows
-    *)       extraDiffArgs='--color=auto' ;;
+    darwin*) extraDiffArgs=() ;;                                 # macOS
+    msys*)   extraDiffArgs=(--color=auto --strip-trailing-cr) ;; # Windows
+    *)       extraDiffArgs=(--color=auto) ;;
   esac
-  # shellcheck disable=SC2086 # We intentionally want word splitting for $extraDiffArgs.
-  diff ${extraDiffArgs} --unified "${dirName}/expected.md" "${dirName}/observed.md" || ((++failures))
+  diff "${extraDiffArgs[@]}" --unified "${dirName}/expected.md" "${dirName}/observed.md" || ((++failures))
 done < <(find "${SOURCE_DIR}" -maxdepth 1 -mindepth 1 -type d -print0 || :)
 [[ "${failures}" -eq 0 ]]
